3930c442507985700fad54af9f13e4241783a67e
Rod <128448506+barorod@users.noreply.github.com>
2024-09-08 02:59:23 +0800

    Add answer on exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (str, multiplier) {
+  if (multiplier < 0) return 'ERROR';
+  let word = '';
+  for (let i = 1; i <= multiplier; i++) {
+    word = word + str;
+  }
+  return word;
 };
 
 // Do not edit below this line
