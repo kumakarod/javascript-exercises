326e6c647ab97ae9687babf65a287425adf099fb
Rod <128448506+barorod@users.noreply.github.com>
2024-09-17 02:30:56 +0800

    Add answer on exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (count) {
+  let a = 0;
+  let b = 1;
+  let fib = 0;
 
+  if (typeof count === 'number') {
+    fib = count;
+  } else {
+    fib = parseInt(count);
+  }
+
+  if (count < 0) return 'OOPS';
+
+  for (let i = 2; i <= count; i++) {
+    fib = a + b;
+    a = b;
+    b = fib;
+  }
+
+  return fib;
 };
 
 // Do not edit below this line
