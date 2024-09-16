2b80495c6ca174d6886404bbb600f135d53d0dd1
Rod <128448506+barorod@users.noreply.github.com>
2024-09-17 01:58:40 +0800

    Add answer on exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const cleanedStr = str.replace(/\W/g, '').toLowerCase();
 
+  const reversedStr = cleanedStr.split('').reverse().join('');
+
+  return cleanedStr === reversedStr;
 };
 
 // Do not edit below this line
